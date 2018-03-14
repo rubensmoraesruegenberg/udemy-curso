1bdc8a917becc4e1f140479aed9e9718d7a272ef
Rubens Moraes Ruegenberg <rubensmoraesruegenberg@gmail.com>
2018-03-13 22:37:48 -0300

    Alterar o formato da moeda, para padrão brasileiro.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
@@ -18,6 +18,7 @@ import { ShoppingCardComponent } from './restaurant-detail/shopping-card/shoppin
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { ShoppingCardService } from './restaurant-detail/shopping-card/shopping-card.service';
+import { OrderComponent } from './order/order.component';
 
 
 @NgModule({
@@ -32,14 +33,15 @@ import { ShoppingCardService } from './restaurant-detail/shopping-card/shopping-
     MenuComponent,
     ShoppingCardComponent,
     MenuItemComponent,
-    ReviewsComponent
+    ReviewsComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService,ShoppingCardService],
+  providers: [RestaurantsService,ShoppingCardService,{provide:LOCALE_ID, useValue:'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
